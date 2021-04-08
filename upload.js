2a0909c795e4a0d35aacf6183e8dbfea4a5a02f1
Benjamin Lannon <benjamin@lannonbr.com>
2021-04-07 20:15:15 -0400

    Only upload images that don't already exist

--- upload.js
+++ upload.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const path = require('path');
 const fileHash = require('md5-file');
 const cloudinary = require('cloudinary').v2;
+const fetch = require('node-fetch')
 
 module.exports = async ({ imagePath, uploadFolder }) => {
   const { name } = path.parse(imagePath);
@@ -14,11 +15,15 @@ module.exports = async ({ imagePath, uploadFolder }) => {
     api_secret: process.env.CLOUDINARY_API_SECRET,
   });
 
-  await cloudinary.uploader.upload(imagePath, {
-    public_id,
-    folder: uploadFolder,
-    overwrite: false,
-  });
+  const resp = await fetch(cloudinary.url(`${uploadFolder}/${public_id}`), { method: 'HEAD' })
+
+  if (resp.status !== 200) {
+    await cloudinary.uploader.upload(imagePath, {
+      public_id,
+      folder: uploadFolder,
+      overwrite: false,
+    });
+  }
 
   return public_id;
 };
